bf4be70cd9b3ad545a427455a1b46838d27e266a
robertoargs <robertoargs@gmail.com>
2026-02-24 13:03:43 -0500

    W5-Custom Document Intelligence

--- 5.SolMineriaConocimiento/mslearn-ai-information-extraction-main/mslearn-ai-information-extraction-main/Labfiles/prebuilt-doc-intelligence/Python/document-analysis.py
+++ 5.SolMineriaConocimiento/mslearn-ai-information-extraction-main/mslearn-ai-information-extraction-main/Labfiles/prebuilt-doc-intelligence/Python/document-analysis.py
@@ -2,8 +2,8 @@ from dotenv import load_dotenv
 import os
 
 # Add references
-
-
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.formrecognizer import DocumentAnalysisClient
 
 def main():
 
@@ -27,14 +27,36 @@ def main():
 
 
         # Create the client
+        document_analysis_client = DocumentAnalysisClient(
+            endpoint=endpoint, credential=AzureKeyCredential(key)
+        )
 
 
 
         # Analyse the invoice
+        poller = document_analysis_client.begin_analyze_document_from_url(
+            fileModelId, fileUri, locale=fileLocale
+        )
 
 
 
         # Display invoice information to the user
+        receipts = poller.result()
+            
+        for idx, receipt in enumerate(receipts.documents):
+            
+            vendor_name = receipt.fields.get("VendorName")
+            if vendor_name:
+                print(f"\nVendor Name: {vendor_name.value}, with confidence {vendor_name.confidence}.")
+
+            customer_name = receipt.fields.get("CustomerName")
+            if customer_name:
+                print(f"Customer Name: '{customer_name.value}, with confidence {customer_name.confidence}.")
+
+
+            invoice_total = receipt.fields.get("InvoiceTotal")
+            if invoice_total:
+                print(f"Invoice Total: '{invoice_total.value.symbol}{invoice_total.value.amount}, with confidence {invoice_total.confidence}.")
 
 
             
